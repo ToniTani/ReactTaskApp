078fe2546a79933cbec835359aa35cd4585520b7
ToniTani <toni.tani@student.saimia.fi>
2021-04-11 06:19:40 +0300

    Initial commit, changing text with a button click

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {Button} from "react-native-web";
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default function App() {
   const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
     <View style={styles.container}>
       <Text>{outputText}</Text>
-      <Button title = "Change text" onPress={() => setOutputText('Hello, text changed!')}/>
+      <Button title = "Change Text" onPress={() => setOutputText('Hello, text changed!')}/>
       <StatusBar style="auto" />
     </View>
   );

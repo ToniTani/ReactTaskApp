c9827f5c93ca94fa998e0dea533a996e1b447af4
ToniTani <toni.tani@student.saimia.fi>
2021-04-13 02:52:34 +0300

    console log testing

--- App.js
+++ App.js
@@ -1,25 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
-  return (
+    const [enteredGoal, setEnteredGoal] = useState('');
+
+
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoal(enteredText);
+
+    };
+
+    const addGoal = () => {
+        console.log(enteredGoal);
+    };
+
+
+    return (
     <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title = "Change Text" onPress={() => setOutputText('Hello, text changed!')}/>
-      <StatusBar style="auto" />
+        <View style={styles.inputContainer}>
+        <TextInput
+            placeholder="Goals"
+            style={styles.input}
+            onChangeText={goalInputHandler}
+            value={enteredGoal}
+        />
+        <Button title = "Add" onPress={addGoal}/>
+        </View>
+      <View/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#40d6ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#40d6ff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 50
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        width: '80%',
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 10
+    }
 });
